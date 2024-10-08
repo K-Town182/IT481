27766209747193900671e7139f35e4803aa1d63c
K-Town182 <kathrynncraig@student.purdueglobal.edu>
2024-10-08 17:27:13 -0500

    Add files via upload

---
+++ Kaytee_Townlain_Unit9_IT481/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kaytee_Townlain_Unit2_IT481
+{
+	internal static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Form1());
+		}
+	}
+}
